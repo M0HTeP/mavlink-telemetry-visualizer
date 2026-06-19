036e72db720f9bb9ca0e9bd4e237ddfbccae914c
Artem Ryazanov <79083447+M0HTeP@users.noreply.github.com>
2026-06-19 11:27:22 +0300

    Delete build directory

--- build/CMakeFiles/mav_visualizer_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mav_visualizer_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mav_visualizer_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/mav_visualizer_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for mav_visualizer_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
